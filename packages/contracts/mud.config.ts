9afabdf6ef192aff7cc2185a9edefbee4391413c
Sanbao Zheng <baobaobao369@163.com>
2023-10-28 20:43:35 +0800

    implement core gameplay

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -12,9 +12,24 @@ export default mudConfig({
     GameEnv: {
       keySchema: {},
       valueSchema: {
+        // global player numbers who has registered
         playerNumber: "uint16",
+        // during how many blocks the player should make his decision for each level
         actionPeriod: "uint16",
-        initialValue: "uint16"
+        // player's initial value 
+        initialValue: "uint16",
+        // every how many levels there will be a robber
+        robberRate: "uint16",
+        // how many levels does one game have
+        levelNumber: "uint16",
+      },
+    },
+    PlayerInfo: {
+      keySchema: {
+        addr: "address",
+      },
+      valueSchema: {
+        uuid: "uint16",
       },
     },
     Player: {
@@ -22,15 +37,16 @@ export default mudConfig({
         addr: "address",
       },
       valueSchema: {
-        uuid: "uint16",
         level: "uint16",
         value: "uint16",
+        // encoding rule: left = uint8 operation | uint16 left_result
+        // operation set = {+, - * /}, where 0 means +, 3 means /
+        // if the player pass through the left gear, then his value will be updated to left_result
         left: "uint24",
         right: "uint24",
         nextLeft: "uint24",
         nextRight: "uint24",
-        robber: "uint16",
-        highestValue: "uint16",
+        robber: "uint32",
         lastUpdate: "uint64",
       },
     },

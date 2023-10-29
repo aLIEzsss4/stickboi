01408191fe59780474665c836361a06261d83f8b
Miguel Lemos <intldds@gmail.com>
2023-10-29 02:18:34 +0000

    update army.tsx and index.tsx (game guidance + door selection)

--- packages/client/src/ui/Army.tsx
+++ packages/client/src/ui/Army.tsx
@@ -1,22 +1,59 @@
-const Arrow = ({ src, content }: { src: string; content: string }) => {
+const Arrow = ({ src, content, onSelect }: { src: string; content: string; onSelect: () => void }) => {
   // TODO: click to select the left door or the right door
-  return (
-    <div className="w-20 m-16">
-      <img src={src} />
-      <div className="whitespace-nowrap w-20"> {content}</div>
-    </div>
-  );
+    return (
+        <div className="w-20 m-16" onClick={onSelect}>
+            <img src={src} alt={content} />
+            <div className="whitespace-nowrap w-20"> {content}</div>
+        </div>
+    );
 };
 
 export const Army = () => {
-  return (
-    <>
-      <div className="flex items-center flex-row">
-        <Arrow src={"./assets/arrow_left.png"} content="Move Left" />
-        {/* TODO: fetch number from chain and stack multiple characters together */}
-        <img src="./assets/stickman.png" alt="Person" className="h-20 my-4" />
-        <Arrow src={"./assets/arrow_right.png"} content="Move Right" />
-      </div>
-    </>
-  );
+    const [characterCount, setCharacterCount] = useState(0);
+    const [selectedDoor, setSelectedDoor] = useState("");
+
+    const handleSelectDoor = (door: string) => {
+        setSelectedDoor(door);
+        // Handle door selection logic here
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const numberFromChain = await fetchNumberFromChain();
+            setCharacterCount(numberFromChain);
+        };
+
+        fetchData();
+    }, []);
+
+    const fetchNumberFromChain = async () => {
+        return Math.floor(Math.random() * 10) + 1;
+    };
+
+    const renderCharacters = () => {
+        const characters = [];
+
+        for (let i = 0; i < characterCount; i++) {
+            characters.push(
+                <img
+                    key={i}
+                    src="./assets/stickman.png"
+                    alt="Person"
+                    className="h-20 my-4"
+                />
+            );
+        }
+
+        return characters;
+    };
+
+    return (
+        <div>
+            <div className="flex items-center flex-row">
+                <Arrow src={"./assets/arrow_left.png"} content="Move Left" onSelect={() => handleSelectDoor("left")} />
+                {renderCharacters()}
+                <Arrow src={"./assets/arrow_right.png"} content="Move Right" onSelect={() => handleSelectDoor("right")} />
+            </div>
+        </div>
+    );
 };
--- packages/client/src/ui/index.tsx
+++ packages/client/src/ui/index.tsx
@@ -3,24 +3,39 @@ import { MockButtons } from "./MockButtons";
 import { Army } from "./army";
 
 const MainPage = () => {
-  return (
-    <div className="flex justify-between">
-      {/* left TODO: more game guidance here */}
-      <div className="flex-grow bg-gray-200"></div>
+    return (
+        <div className="flex justify-between">
+            {/* left TODO: more game guidance here */}
+            <div className="flex-grow bg-gray-200">
+                <div className="p-4">
+                    <h2 className="text-xl font-semibold mb-2">Game Instructions</h2>
+                    <p>
+                        Welcome to the game! Choose the right door and get your memes!
+                    </p>
+                </div>
+            </div>
 
-      {/* middle */}
-      <div className="w-112 bg-white-300">
-        <div className="flex flex-col justify-center items-center h-screen">
-          <Doors />
-          <Army />
-          <MockButtons />
+            {/* middle */}
+            <div className="w-112 bg-white-300">
+                <div className="flex flex-col justify-center items-center h-screen">
+                    <Doors />
+                    <Army />
+                    <MockButtons />
+                </div>
+            </div>
+
+            {/* right TODO: more game guidance here */}
+            <div className="flex-grow bg-gray-200">
+                <div className="p-4">
+                    <h2 className="text-xl font-semibold mb-2">Rankings/Points</h2>
+                    <ul>
+                        <li>The highest number the player reach.</li>
+                        <li>How many doors player pass.</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-
-      {/* right TODO: more game guidance here */}
-      <div className="flex-grow bg-gray-200"></div>
-    </div>
-  );
+    );
 };
 
 export default MainPage;

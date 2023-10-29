9153d6aad7f32576386c67f289da896afc63f0e7
Noy <59097122+noyyyy@users.noreply.github.com>
2023-10-30 04:03:40 +0800

    docs: add rule back

--- packages/client/src/ui/index.tsx
+++ packages/client/src/ui/index.tsx
@@ -1,27 +1,53 @@
-import {Middle} from "./Middle";
+import { Middle } from "./Middle";
+import { Rank } from "./Rank";
 
 const MainPage = () => {
   return (
     <div className="flex justify-between">
-      {/* left TODO: more game guidance here */}
-      <div className="flex-grow bg-gray-200">
+      <div className="flex-grow bg-gray-200 w-1/3">
         <div className="p-4">
-          <h2 className="text-xl font-semibold mb-2">Game Instructions</h2>
-          <p>Welcome to the game! Choose the right door and get your memes!</p>
+          <h2 className="text-xl font-semibold mb-2">Gameplay</h2>
+          <p>
+            Start as one player, move forward, and face two doors with random
+            numbers. Choose a door to pass. Points are calculated based on the
+            number on the door. The game ends if the number is negative or
+            manually by the player.
+          </p>
+          <h2 className="text-xl font-semibold mb-2">
+            Quantity Conversion Rule
+          </h2>
+          <p>
+            1 wojak = 5 stickboi
+            <br />
+            1 Mefer = 25 stickboi
+            <br />
+            1 Pepe = 50 stickboi
+            <br />
+            1 Milady = 125 stickboi
+            <br />
+            1 Shiba = 500 stickboi
+            <br />
+            1 Nouns = 625 stickboi
+            <br />
+            1 Dog = 5000 stickboi
+            <br />
+            Number 18532 = 3 Dog + 5 Nouns + 3 Milady + 1 Mefer + 1 wojak + 2
+            stickboi
+          </p>
         </div>
       </div>
 
       {/* middle */}
       <Middle />
 
-      {/* right TODO: more game guidance here */}
-      <div className="flex-grow bg-gray-200">
+      <div className="flex-grow bg-gray-200 w-1/4">
         <div className="p-4">
           <h2 className="text-xl font-semibold mb-2">Rankings/Points</h2>
           <ul>
             <li>The highest number the player reach.</li>
             <li>How many doors player pass.</li>
           </ul>
+          <Rank />
         </div>
       </div>
     </div>
